81e48533c24a8f063f23bf878a03185d271f6078
Christopher West <9ninemind@gmail.com>
2015-04-16 11:30:46 -0600

    adds new entities

---
+++ www/src/AppBundle/Entity/RegionZone.php
@@ -0,0 +1,12 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class RegionZone
+{
+    use \BlackBoxCode\Pando\Bundle\ContactInfoBundle\Model\RegionZoneTrait;
+}
---
+++ www/src/AppBundle/Entity/RegionZoneType.php
@@ -0,0 +1,12 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class RegionZoneType
+{
+    use \BlackBoxCode\Pando\Bundle\ContactInfoBundle\Model\RegionZoneTypeTrait;
+}
