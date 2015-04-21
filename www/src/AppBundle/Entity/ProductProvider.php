5586c4d003d2fe5dc146c97d619f50eaeeddb22b
Christopher West <9ninemind@gmail.com>
2015-04-21 13:48:20 -0600

    adds transaction path interface

--- www/src/AppBundle/Entity/ProductProvider.php
+++ www/src/AppBundle/Entity/ProductProvider.php
@@ -1,12 +1,15 @@
 <?php
 namespace AppBundle\Entity;
 
+use BlackBoxCode\Pando\Bundle\ProductSaleBundle\Model\TransactionPathResultInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  */
-class ProductProvider implements \BlackBoxCode\Pando\Bundle\ProductProviderBundle\Model\ProductProviderInterface
+class ProductProvider implements
+    TransactionPathResultInterface,
+    \BlackBoxCode\Pando\Bundle\ProductProviderBundle\Model\ProductProviderInterface
 {
     use \BlackBoxCode\Pando\Bundle\ProductProviderBundle\Model\ProductProviderTrait;
 }
